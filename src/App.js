55fcef000bc56fd9798441696cf0063ffa5f5d72
ShuvamMazumder <shuvammaz94@gmail.com>
2022-03-12 20:00:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+//Basic Dynamic content load
+/*function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Friend phone="019" address='dhk'></Friend>
+      <Friend phone="017" address="com"></Friend>
+      <Friend phone="019" address="ctg"></Friend>
     </div>
   );
 }
+function Friend(props){
+  return(
+    <div>
+      <h3>phone: {props.phone}</h3>
+      <h4>address: {props.address}</h4>
+    </div>
+  );
+}
+*/
+
+//Basic Dynamic content load using Array
+
+/*function App(){
+  const casts = [
+    {hero:'a',heroin:'b'},
+    {hero:'c',heroin:'d'},
+    {hero:'e',heroin:'f'},
+    {hero:'g',heroin:'h'},
+  ]
+  return (
+    <div className="App">
+    {
+      casts.map(casts=> <Show hero={casts.hero} heroin={casts.heroin}></Show>)
+    }
+    </div>
+  );
+}
+
+function Show (props){
+  return (
+    <div>
+      <h2>Hero:{props.hero} </h2>
+      <h3>heroin:{props.heroin} </h3>
+    </div>
+  );
+}
+*/
+
+//Learning State
+
+
 
 export default App;
